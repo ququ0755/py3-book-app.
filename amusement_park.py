d2fa07ff34a1bb635ba7c6e1b8b37e3d245935db
Your Name <ququ@qucmpution.onmicrosoft.com>
2020-10-13 19:49:53 +0800

    new file

--- amusement_park.py
+++ amusement_park.py
@@ -6,3 +6,24 @@ elif age < 18:
     print("Your admission cost is $5.")
 else:
     print("Your admission cost is $10.")        
+
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+else:
+    price = 10
+
+print("Your admission cost is $" + str(price) + ".")
+
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+else:
+    price = 5    
+print("Your admission cost is $" + str(price) + ".")
