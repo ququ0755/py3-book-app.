dd6d7a38168f9aa00ecdd5b2e9743e89c24b2564
Your Name <ququ@qucmpution.onmicrosoft.com>
2022-02-24 11:42:59 +0800

    new file

--- 随机抽取.py
+++ 随机抽取.py
@@ -1,2 +1,12 @@
+import sys
 import random
-print(random.randint(1,47))
+user_input = input("Please enter some text, and I will display it(enter 继续 to continue 终止 to exit): ")
+if user_input == "继续":
+    print(random.randint(1,47))
+else:
+    if user_input == "终止":
+        sys.exit()
+        sys.exit(0)
+  
+
+
